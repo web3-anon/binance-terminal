7b00237828cc193179032c0958842fb32c8dfa84
web3-anon <124695213+web3-anon@users.noreply.github.com>
2023-02-08 19:48:01 +0500

    Create lastWithdrawals.py

---
+++ lastWithdrawals.py
@@ -0,0 +1,11 @@
+def displayWithdrawals(z):
+    print("\n------------------------------------------\n")
+    print("Last Withdrawal:\n")
+    print(z[0])
+    print("\n------------------------------------------\n")
+    print("\nSecond Last Withdrawal:\n")
+    print(z[1])
+    print("\n------------------------------------------\n")
+    print("\nThird Last Withdrawal:\n")
+    print(z[2])
+    print("\n------------------------------------------\n")
